ce52814398250f59b77acabfea45d9cace47ed29
Lehlogonolo Sylvester <lehlogonolo.vester@gmail.com>
2026-04-18 12:07:03 +0200

    Complete demo payroll system v1.1

--- main.py
+++ main.py
@@ -4,20 +4,32 @@
 
 # QUESTION 1: 
 # Create three variables to get user input (floats):
-# - monthly_gross_salary
-# - medical_aid_premium
-# - num_dependents (for medical credits)
 
+#monthly_gross_salary
+monthly_gross_salary = float(input("insert gross salary: R"))
+# - medical_aid_premium
+medical_aid_premium = float(input("med aid premium: R")) 
+# - num_dependents (for medical credits)
+num_dependants = (input("how many dependants on your med aid?:"))
+#print(f"your gross salary is 'R'{monthly_gross_salary} while your med aid premiums are 'R'{medical_aid_premium} with {num_dependants} dependants ")
+#print(f"your gross salary is R{monthly_gross_salary:.2f} while your medical aid premium is R{medical_aid_premium:.2f} with {num_dependants} dependants")
 
 # QUESTION 2:
 # Calculate the monthly UIF contribution. 
 # Remember: It is 1% of gross salary, but it is capped at R177.12.
 # Hint: Use an 'if' statement or the min() function.
-
+uif = monthly_gross_salary * 0.01
+if uif > 177.12:
+    uif = 177.12
+print("Monthly UIF Contribution:", uif)
 
 # QUESTION 3:
-# To calculate tax (PAYE), we need the annual salary.
+# To calculate tax (PAYE), we need the annual salary. 
+annual_gross = monthly_gross_salary * 12
+PAYE = float(monthly_gross_salary * 0.18)
 # Create a variable 'annual_gross' by multiplying monthly salary by 12.
+print("Annual Tax (PAYE):", PAYE)
+
 
 
 # QUESTION 4:
@@ -26,12 +38,43 @@
 # Example: 
 # If income <= 245100, tax is 18%.
 # If income > 245100 and <= 383100, tax is 44118 + 26% of amount above 245100.
+if annual_gross <= 245100:
+
+    base_tax = annual_gross * 0.18
+
+elif annual_gross > 245100 and annual_gross <= 383100:
+
+    base_tax = 44118 + (annual_gross - 245100) * 0.26
+
+elif annual_gross > 383100 and annual_gross <= 531600:
+
+    base_tax = 80078 + (annual_gross - 383100) * 0.31
+
+elif annual_gross <= 673000:
+
+    base_tax = 126475 + (annual_gross - 531600) * 0.36
+
+elif annual_gross <= 857900:
+
+    base_tax = 177147 + (annual_gross - 673000) * 0.39
+
+elif annual_gross <= 1817000:
+
+    base_tax = 251258 + (annual_gross - 857900) * 0.41
+
+else:
+
+    base_tax = 644489 + (annual_gross - 1817000) * 0.45
+print("question 5: base_tax")
 
 
 # QUESTION 5:
 # Everyone gets a Primary Rebate of R17,820 per year.
 # Subtract this rebate from your 'base_tax'. 
 # Note: Tax cannot be less than zero!
+primary_rebate = 17820
+rebate = base_tax - primary_rebate
+print ("rebate:", rebate)
 
 
 # QUESTION 6:
@@ -39,13 +82,26 @@
 # For 2026, the main member gets R376 off per month.
 # Calculate the 'monthly_tax' by dividing annual tax by 12, 
 # then subtract the R376 credit.
-
+MONTHLY_MTC = 376
+monthly_tax =  (base_tax / 12) - MONTHLY_MTC
+print("Monthly Tax after Medical Tax Credit:", monthly_tax) 
 
 # QUESTION 7:
 # Final Step! Calculate the 'net_salary'.
 # Formula: Gross - Monthly Tax - UIF - Medical Aid Premium.
+# --- Net Salary ---
+net_salary = monthly_gross_salary - monthly_tax - uif - medical_aid_premium
+print("Net Salary:", net_salary)
 
 
 # QUESTION 8:
 # Print a professional payslip showing:
 # Gross Salary, UIF Deduction, Tax Paid, and the final Net Salary.
+monthlytax=PAYE/12
+print("----------Payslip----------")
+print("monthly gross:",monthly_gross_salary)
+print("uif deductions:", uif)
+print("pay as you earn:",monthlytax)
+print("med aid contribution:",medical_aid_premium)
+print("--------------------")
+print("net salary:", f"R{net_salary:.2f}")
